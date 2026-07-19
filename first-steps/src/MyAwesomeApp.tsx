d00a41b1f4ee810569f3eee563a5e818213bd185
Cris Franco <francochavezcristian@gmail.com>
2026-07-19 00:01:13 -0600

    Printing arrrays, objects, etc

--- first-steps/src/MyAwesomeApp.tsx
+++ first-steps/src/MyAwesomeApp.tsx
@@ -1,11 +1,25 @@
 export function MyAwesomeApp(){
     const firstName = "Cris";
     const lastName = "Franco";
+    const favoriteGames = ['Elden Ring', 'smash', 'Metal Gear'];
+    const isActive = true;
+    const address = {
+        zipCode: 'ABC-123',
+        country: 'Canada'
+
+    };
 
     return (
         <>
         <h1>{firstName}</h1>
         <h3>{lastName}</h3>
+
+        <p>{1+1}</p>
+
+        <h3>{favoriteGames.join(',')}</h3>
+        <h2>{isActive? 'Active': 'Inactive'}</h2>
+        <h3>{JSON.stringify(address)}</h3>
+
         </>
     );
 }
