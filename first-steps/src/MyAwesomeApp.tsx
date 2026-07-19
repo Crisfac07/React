0f995efb3eb46c12afba56355e07bfc6e643fb0c
Cris Franco <francochavezcristian@gmail.com>
2026-07-18 23:53:30 -0600

    Printing variables in component

--- first-steps/src/MyAwesomeApp.tsx
+++ first-steps/src/MyAwesomeApp.tsx
@@ -1,9 +1,11 @@
 export function MyAwesomeApp(){
+    const firstName = "Cris";
+    const lastName = "Franco";
 
     return (
         <>
-        <h1>Cris</h1>
-        <h3>Franco</h3>
+        <h1>{firstName}</h1>
+        <h3>{lastName}</h3>
         </>
     );
 }
